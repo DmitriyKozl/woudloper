1acd9789e9ba2c09d28790531abaf9a87b1388b3
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2021-03-15 14:04:08 +0100

    changes main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/style/style.scss';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUserSecret);
+// library.add(faUserSecret);
 
 // createApp.component('font-awesome-icon', FontAwesomeIcon);
 
